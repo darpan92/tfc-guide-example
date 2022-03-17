36f9ae34770f06c6c14489e18525550dd73ba8a6
darpan92 <darpandpatel@gmail.com>
2022-03-17 14:10:43 -0400

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -8,7 +8,10 @@ terraform {
 }
 
 resource "aws_instance" "web" { 
-  ami = "ami-0c02fb55956c7d316"
+  ami = var.ami
   instance_type = var.instance_type
+  tags = {
+    Name = var.instance_name
+  }
 }
 
